48020daaa36bde3ff1dc98b7e18b70c1d9bfbd2d
Vitaly Osipov <vital-nvl@mail.ru>
2022-09-11 14:57:46 +0300

    Sum digits

--- Work2/Program.cs
+++ Work2/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+int SumDigits(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + (num % 10);
+        num = num / 10;
+    }
+    return sum;
+}
+
+
+try
+{
+    Console.Write("Введите число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    int sum = SumDigits(num);
+    Console.WriteLine($"Сумма цифр в числе {num} равна {sum} ");
+}
+catch
+{
+    Console.WriteLine("Введено не число!!! "); 
+}
+
