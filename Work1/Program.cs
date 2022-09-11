432774eb7533f86d521b65b39722612e02818c62
Vitaly Osipov <vital-nvl@mail.ru>
2022-09-11 14:14:38 +0300

    A pow b

--- Work1/Program.cs
+++ Work1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B. 
+// Решить задачу с использованием методов.
+
+int Degree(int a, int b)
+{
+    int result = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        result = result * a;
+    }
+    return result;
+}
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите степень числа: ");
+int b = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+int result = Degree(a, b);
+Console.WriteLine($"Число {a} в степени {b} равно {result}: ");
