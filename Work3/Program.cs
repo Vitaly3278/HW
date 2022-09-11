6c838513076efaa38f1d6653b94498ea7c61d7e7
Vitaly Osipov <vital-nvl@mail.ru>
2022-09-11 16:20:58 +0300

    Get Array

---
+++ Work3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
+// Элементы массива вводятся пользователем.
+
+int [] GetArray (int x)
+{
+    int [] array = new int [x];
+    for (int i =0; i < x; i++)
+    {
+        Console.Write($"Введите {i+1} число массива: ");
+        array[i] = int.Parse(Console.ReadLine());
+    }
+    return array;
+}
+
+Console.Write("Введите размер массива: ");
+try{
+    int x = int.Parse(Console.ReadLine());
+    int [] result = GetArray(x);
+    Console.WriteLine($"Ваш массив: [{string.Join(",", result)}]");
+}
+catch
+{
+    Console.WriteLine("ОШИБКА! Вы ввели не число!");
+}
